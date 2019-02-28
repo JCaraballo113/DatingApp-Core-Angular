1e8a863eeb57bb2b75d657ed70f6fb94412e123a
John Caraballo <jcaraballo019@gmail.com>
2019-02-27 23:23:02 -0400

    Implemented front-end navbar component with login

---
+++ DatingApp-SPA/src/app/Navbar/Navbar.component.ts
@@ -0,0 +1,24 @@
+import { AuthService } from './../_services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Navbar',
+  templateUrl: './Navbar.component.html',
+  styleUrls: ['./Navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  async login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login')
+    });
+  }
+}
